9ee951323b78d25724342a088de0555254b50d6c
Scott Hall <scotthall127@gmail.com>
2018-04-20 04:10:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,11 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string)
   return "I can't hear you!"
 }
+
+function sayHiToGrandma(string) {
+  if (string.toUpperCase() === string)
+  return "YES INDEED!"
+}
+
+
+}
