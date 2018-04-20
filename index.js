f9a83757a258c3aa93bab29129aec55061d5e2cd
Scott Hall <scotthall127@gmail.com>
2018-04-20 04:22:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toUpperCase() === string)
+  if (string.toLowerCase() === string)
   return "I can't hear you!"
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string)
+  if (string.toUpperCase() === string)
   return "YES INDEED!"
 }
   
